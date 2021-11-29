e823846e736c94a5ff749001086bcec76b8d21d3
BenjaminOrtizJr <bortizjr84@outlook.com>
2021-11-29 11:55:01 -0500

    week 1 assignment

--- week1/assignment_1/payroll2.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    
-    def __init__(self, firstName, lastName, employeeID, hourlyPay):
-        self.firstName = input("Enter first name: ")
-        self.lastName = input("Enter last name: ")
-        self.employeeID = int(input("Enter employee id: "))
-        self.hourlyPay = float(input("Enter hourly rate: "))
-        
-    
