8c0c35b5aaa9a2f1af7fedd713f08005647c5573
BenjaminOrtizJr <bortizjr84@outlook.com>
2021-12-06 15:05:11 -0500

    Project 1: Recursion (2nd Attempt)

--- week2/project_1/recursion.py
+++ week2/project_1/recursion.py
@@ -7,6 +7,14 @@ def loop1():
             odd_sum += i
     return odd_sum
 
+def loop1Rec(num,odd_sum):
+    if num == 20:
+        return odd_sum
+    else:
+        if (num % 2) == 1:
+            odd_sum += num
+    return loop1Rec((num + 1), odd_sum)
+
 def loop2():
     # Sum the even numbers between 1 and 20
     i = 0
@@ -16,27 +24,19 @@ def loop2():
             even_sum += i
         i += 1
     return even_sum
-
-def loop1Rec(num,odd_sum):
-    odd_sum = 0
-    if num == 20:
-        return odd_sum
-    else:
-        if (num % 2) == 1:
-            return loop1Rec((num + 1) + odd_sum)
     
    
 def loop2Rec(num,even_sum):
-    num = 0
-    even_sum = 0
     if (num==20):
         return even_sum
     else:
         if (num % 2) == 0:
-            return loop2Rec((num + 1) + (even_sum + num))
+            even_sum += num
+        num += 1
+    return loop2Rec((num + 1), even_sum)
 
 
-print(loop1())
-print(loop2())
-print(loop1Rec())
-print(loop2Rec())
+print("Sum of odds between 1 and 20 using 'for' loop = " + str(loop1()))
+print("Sum of odds between 1 and 20 using recursion = " + str(loop1Rec(0,0)))
+print("Sum of evens between 1 and 20 using 'while loop = " + str(loop2()))
+print("Sum of evens between 1 and 20 using recursion = " + str(loop2Rec(0,0)))
