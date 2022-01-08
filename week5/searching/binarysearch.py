9fdd4c49d71f79dafa0f05a630d1215c66f9ff8f
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2022-01-08 18:01:21 -0500

    Week 5 Assignment 5.1: Searching

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -7,17 +7,20 @@ def binary_search(sorted_list, search_term):
     while first_index <= last_index:
         midpoint = (first_index + last_index) // 2
         
-        print(midpoint)
         if sorted_list[midpoint] == search_term:
             return True
         
         elif search_term > sorted_list[midpoint]:
             first_index = midpoint + 1
-        
             
-    if first_index > last_index:
-        return False
+        elif search_term < sorted_list[midpoint]:
+            last_index = midpoint - 1
+        
+    return False
     
 myList = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
+myList2 = ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]
 
 print(binary_search(myList, 7))
+print(binary_search(myList, 77))
+print(binary_search(myList2, "Delta"))
